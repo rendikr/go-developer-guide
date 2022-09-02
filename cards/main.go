06fb028c6bf1d87e9adeb75a40ec8e714fb449c0
Rendi K <rendi.krinaldi@gmail.com>
2022-09-02 07:43:32 +0700

    how to declare a variable

---
+++ cards/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// declare a variable
+
+	// this one works
+	// var card string = "Ace of Spades"
+
+	// this one also works
+	card := "Ace of Spades"
+
+	// reassign a new value to a variable
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
+}
