3b6a442466d24ab032d58a06ba5e72b59953a12a
Rendi K <rendi@sayakaya.id>
2022-09-06 09:46:51 +0700

    create a new deck

--- cards/main.go
+++ cards/main.go
@@ -4,17 +4,7 @@ func main() {
 	// declare a new slice of type string
 	// slice is a type of array that can grow or shrink, must have the same type of value
 	// array is fixed length list
-	cards := deck{
-		newCard(),
-		"Five of Diamonds",
-	}
-
-	// append a new value to slice
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
 	cards.print()
-}
-
-func newCard() string {
-	return "Ace of Spades"
 }
