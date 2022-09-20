3c83eb86bfe26195155b52985f4e780357be745f
Rendi K <rendi@sayakaya.id>
2022-09-20 11:22:17 +0700

    reading from hard drive

--- cards/main.go
+++ cards/main.go
@@ -1,19 +1,19 @@
 package main
 
-import "fmt"
-
 func main() {
 	// declare a new slice of type string
 	// slice is a type of array that can grow or shrink, must have the same type of value
 	// array is fixed length list
-	cards := newDeck()
+	// cards := newDeck()
 
-	fmt.Println(cards.toString())
+	// fmt.Println(cards.toString())
 
-	hand, remainingCards := deal(cards, 5)
+	// hand, remainingCards := deal(cards, 5)
 
-	hand.print()
-	remainingCards.print()
+	// hand.print()
+	// remainingCards.print()
 
-	cards.saveToFile("my_cards")
+	// cards.saveToFile("my_cards")
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
