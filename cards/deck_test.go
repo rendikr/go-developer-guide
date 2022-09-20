9dc909b3647fc8fbb6a64019da2067909edd996e
Rendi K <rendi@sayakaya.id>
2022-09-20 13:33:38 +0700

    asserting elements in slice

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -8,4 +8,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 20 {
 		t.Error("Expected deck length of 20, but got ", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Error("Expected first card of Ace of Spades, but got ", d[0])
+	}
+
+	if d[len(d)-1] != "Five of Clubs" {
+		t.Error("Expected last card of Five of Clubs, but got ", d[len(d)-1])
+	}
 }
